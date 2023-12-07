ca5a56eea82bd3dcbbfa8c192064b591e9e44ec6
vikhri <vikhrova.irina@gmail.com>
2023-12-07 18:42:23 +0300

    add dist files

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import './style.css'
 
+const openMenuButton = document.querySelector(".js-menu-open-button");
+const closeMenuButton = document.querySelector(".js-menu-close-button");
+const menu = document.querySelector(".js-menu");
 
 const images = document.querySelectorAll(".js-anim-image");
 
@@ -16,4 +19,25 @@ images.forEach((img) => {
     img.style.transform = 'translate(0, 0)';
   });
 
-})
+});
+
+function openMenu() {
+  menu.classList.add("open");
+  document.addEventListener("click", closeMenuOutside);
+}
+
+function closeMenu() {
+  menu.classList.remove("open");
+  document.removeEventListener("click", closeMenuOutside);
+}
+
+function closeMenuOutside(event) {
+  if (!menu.contains(event.target) && event.target !== openMenuButton) {
+    closeMenu();
+  }
+}
+
+
+openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
+
